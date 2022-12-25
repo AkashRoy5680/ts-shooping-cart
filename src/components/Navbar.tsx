7355ee47bcd17236a762c86c170fcef5a4eaf3f5
AkashRoy5680 <royakash1272@gmail.com>
2022-12-25 20:13:01 +0100

    first commit

---
+++ src/components/Navbar.tsx
@@ -0,0 +1,8 @@
+import {Container, Navbar as NavbarBs} from "react-bootstrap"
+export function Navbar() {
+    return( 
+    <NavbarBs className="bg-white shadow-sm mb-3">
+       <Container>Navigation Bar</Container>
+    </NavbarBs>
+    )
+}
